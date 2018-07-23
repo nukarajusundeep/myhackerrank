b58b74c6f6af76a144dcb8b61d3ba5dc7b37636e
vnukaraju <sundeepnukaraju@gmail.com>
2018-07-22 18:47:03 -0700

    Polynomial Input and verify the output

--- input.py
+++ input.py
@@ -1,5 +1,24 @@
+'''
+TASK:
+=========
+You are given a polynomial  of a single indeterminate (or variable), .
+You are also given the values of  and . Your task is to verify if .
+'''
+
+
+def eval_polynomial(poly, val):
+    xs = [ x.strip().replace('^','**') for x in poly.split('+') ]
+    return sum( [eval(n.replace('x', str(val))) for n in xs] )
+
+
 try:
     x, k = [int(x) for x in input().split()]
+    p = input()
+    poly = eval_polynomial(p, x)
+    if poly == k:
+        print(True)
 
+    else:
+        print(False)
 except ValueError:
     print("Enter substitute Variable and output")
