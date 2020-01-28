e10b1c7c8131b03a4d59f52edeb1b514e65af3bd
nvssd <sundeepnukaraju@gmail.com>
2020-01-27 18:44:31 -0800

    Arthematic, division, loops solutions

---
+++ Solutions/loops.py
@@ -0,0 +1,31 @@
+'''Check Tutorial tab to know how to to solve.
+
+Task
+Read an integer . For all non-negative integers , print . See the sample for details.
+
+Input Format
+
+The first and only line contains the integer, .
+
+Constraints
+
+
+Output Format
+
+Print  lines, one corresponding to each .
+
+Sample Input 0
+
+5
+Sample Output 0
+
+0
+1
+4
+9
+16'''
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(0, n):
+        print(i**2)
